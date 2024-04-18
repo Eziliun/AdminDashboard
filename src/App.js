2a72180bb186c0c60c636e2cf095170df92c43fc
Wilian Dimitri <wilianw2011@hotmail.com>
2024-04-18 15:07:58 -0300

    NavBar feita e correção de funcionalidades

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css'
 
-const App = () => {
 
-  const activeMenu = true
+import { useStateContext } from './contexts/ContextProvider';
+
+const App = () => {
+  const { activeMenu } = useStateContext()
+
   return (
     <div>
       <BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,11 +5,17 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { links } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
 
+  const { activeMenu, setActiveMenu, screenSize }  = useStateContext();
 
-  const activeMenu = true
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -17,11 +23,11 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
       <div className='flex justify-between items-center'>
-        <Link to = "/" onClick={() => {}} className="items-center gap-3 ml-3 mt-4 flex text-2xl font-extrabold tracking-tight dark:text-white text-slate-900"> 
+        <Link to = "/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-2xl font-extrabold tracking-tight dark:text-white text-slate-900"> 
           <SiShopware /> <span>Shoppy</span>
         </Link>
         <TooltipComponent content="Menu" position='BottomCenter'>
-            <button type='button' onClick={() => {}} className='text-2xl rounded-full p-3 hover:bg-light-gray mt-4 block'>
+            <button type='button' onClick={() => setActiveMenu(!activeMenu)} className='text-2xl rounded-full p-3 hover:bg-light-gray mt-4 block'>
               <MdOutlineCancel /> 
             </button>
         </TooltipComponent>
@@ -36,7 +42,7 @@ const Sidebar = () => {
                 <NavLink 
                 to={`/${link.name}`}
                 key={link.name}
-                onClick={() => {}}
+                onClick={handleCloseSideBar}
                 className={({ isActive}) => isActive ? activeLink : normalLink }
                 >
                   {link.icon}
