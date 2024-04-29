d525c6485fab84dcd58d283229478a1f58950f58
Wilian Dimitri <wilianw2011@hotmail.com>
2024-04-29 17:54:01 -0300

    Implementando o visual do ThemmeSetting

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -23,9 +23,53 @@ const ThemeSettings = () => {
         </div>
         <div className='flex-col border-t-1 border-color p-4 ml-4'>
           <p className='font-semibold text-lg'>Theme Options</p>
+
+          <div className='mt-4'>
+            <input 
+            type="radio" 
+            id='light'
+            name='theme'
+            value="Light"
+            className='cursos-pointer'
+            onChange={() => {}}
+            checked={true}
+            />
+            <label htmlFor="light" className='ml-2 text-md cursor-pointer'>Light</label>
+          </div>
+
+          <div className='mt-4'>
+            <input 
+            type="radio" 
+            id='dark'
+            name='theme'
+            value="Dark"
+            className='cursos-pointer'
+            onChange={() => {}}
+            checked={true}
+            />
+            <label htmlFor="dark" className='ml-2 text-md cursor-pointer'>Dark</label>
+          </div>
         </div>
+        <div className='flex-col border-t-1 border-color p-4 ml-4'>
+          <p className='font-semibold text-lg'>Theme Colors</p>
+          <div className='flex gap-3'>
+            {themeColors.map((item, index) => (
+              <TooltipComponent key={index} content={item.name} position="TopCenter" >
+                  <div className='relative mt-2 cursor-pointer flex gap-5 items-center'>
+                    <button
+                    type='button'
+                    className='h-10 w-10 rounded-full cursor-pointer'
+                    style={{backgroundColor: item.color}}
+                    onClick={() => {}}
+                    >
+                    <BsCheck className={`ml-2 text-2xl text-white ${true ? 'block' : 'hidden'}`} />
+                    </button>
+                  </div>
+              </TooltipComponent>
+            ))}
+          </div>
+          </div>
       </div>
-      
     </div>
   )
 }
