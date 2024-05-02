8809d07c0b14630fd13e118f7a8f6958eef48cd4
Wilian Dimitri <wilianw2011@hotmail.com>
2024-05-02 18:00:28 -0300

    Adição dos Charts

--- src/App.js
+++ src/App.js
@@ -64,31 +64,29 @@ const App = () => {
               {themeSettings && (<ThemeSettings />)}
 
               <Routes>
-                {/* dashboard  */}
-                <Route path="/" element={(<Ecommerce />)} />
+              <Route path="/" element={(<Ecommerce />)} />
                 <Route path="/ecommerce" element={(<Ecommerce />)} />
 
                 {/* pages  */}
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/employees" element={<Employees />} />
-                <Route path="/costumers" element={<Costumers />} />
+                <Route path="/pedidos" element={<Orders />} />
+                <Route path="/colaboradores" element={<Employees />} />
+                <Route path="/clientes" element={<Costumers />} />
 
                 {/* apps  */}
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
-                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/calendário" element={<Calendar />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
 
                 {/* charts  */}
-                <Route path="/line" element={<Line />} />
+                <Route path="/linha" element={<Line />} />
                 <Route path="/area" element={<Area />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/financial" element={<Financial />} />
-                <Route path="/color-mapping" element={<ColorMapping />} />
-                <Route path="/pyramid" element={<Pyramid />} />
-                <Route path="/stacked" element={<Stacked />} />
-
+                <Route path="/barra" element={<Bar />} />
+                <Route path="/torta" element={<Pie />} />
+                <Route path="/financeira" element={<Financial />} />
+                <Route path="/mapa area" element={<ColorMapping />} />
+                <Route path="/pirâmide" element={<Pyramid />} />
+                <Route path="/pilha" element={<Stacked />} />
               </Routes>
             </div>
             <Footer />
